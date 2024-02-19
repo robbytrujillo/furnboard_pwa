e10a310839685013783474dea8925c78bb0dec79
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-02-20 05:56:10 +0700

    show modal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="flex justify-center px-4 py-8 text-center">
-      <p className="text-sm">Copyright 2024 • All Rights Reserved LuxSpace by Robby Ilham.</p>
+      <p className="text-sm">Copyright 2024 • All Rights Reserved furnBoard by Robby Ilham.</p>
     </footer>
   );
 }
